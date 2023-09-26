d82676bcc4046e2410f1f5b36188e6f6e7b2cf12
DubaLMAO <146026354+DubaLMAO@users.noreply.github.com>
2023-09-26 12:29:51 -0600

    Add files via upload

---
+++ Proyecto/Suma.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int a;
+    int b;
+    int res;
+
+    cout << "Ingresa el primer número" << endl;
+    cin >> a;
+
+    cout << "Ingresa el segundo numero" << endl;
+    cin >> b;
+
+    res = a + b;
+
+    cout << "El resultado de la suma es: " << res << endl;
+
+    return 0;
+
+}
